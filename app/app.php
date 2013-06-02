6f2c5b3789e6820d8459cccd1ba1ab68a135f530
sztart@gmail.com <sztart@gmail.com>
2013-06-02 18:13:54 +0200

    Remove main page - use oferta instead

--- app/app.php
+++ app/app.php
@@ -1,52 +1,55 @@
 <?php
+use Symfony\Component\HttpFoundation\Request;
+use Silex\Application;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 //bootstrap and configuration
-$app = new Silex\Application();
+$app = new Application();
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views/',
+    'twig.path' => __DIR__ . '/views/',
 ));
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
-$dispatcher = function ($url = '') use ($app)  {
+$dispatcher = function (Application $app, Request $request, $url = '') {
 
-	$html = '';
+    $response = '';
 
-	if (1 === preg_match('/[a-z\/]/', $url)) {
-		$templatePath = $url .'.html.twig';
-		$html = $app['twig']->render($templatePath);
-	} elseif ('' === $url) {
-		$html = $app['twig']->render('index.html.twig');
-	}
+    if (1 === preg_match('/[a-z\/]/', $url)) {
+        $templatePath = $url . '.html.twig';
+        $response = $app['twig']->render($templatePath);
+    } elseif ('' === $url) {
+        $uri = $request->getUriForPath('/oferta');
+        $response = $app->redirect($uri);
+    }
 
-	return $html;
+    return $response;
 };
 
 $app->get('/', $dispatcher);
 $app->get('/{url}', $dispatcher)->assert('url', '.+')->bind('page');
 
-$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
+$app['twig'] = $app->share($app->extend('twig', function ($twig, $app) {
 
-	$function = new Twig_SimpleFunction('ifUrlStartsWithThenReturn', function ($start, $return = '', $else = '') use ($app) {
-		$url = strtolower($app['request']->attributes->get('url'));
-		$start = strtolower($start);
-		$out = $else;
+    $function = new Twig_SimpleFunction('ifUrlStartsWithThenReturn', function ($start, $return = '', $else = '') use ($app) {
+        $url = strtolower($app['request']->attributes->get('url'));
+        $start = strtolower($start);
+        $out = $else;
 
-		if (strpos($url, $start) === 0) {
-			$out = $return;
-		}
+        if (strpos($url, $start) === 0) {
+            $out = $return;
+        }
 
-		return $out;
-	});
-	$twig->addFunction($function);
-	$twig->addGlobal('baseHostAndUrl', $app['request']->getUriForPath('')); //trick to run without vhost
+        return $out;
+    });
+    $twig->addFunction($function);
+    $twig->addGlobal('basePath', $app['request']->getUriForPath('')); //trick to run without vhost
 
-	return $twig;
+    return $twig;
 }));
 
-$app->error(function(\Exception $e) use ($app) {
-	return 'error';
+$app->error(function (\Exception $e) use ($app) {
+    return 'error';
 });
 
 return $app;
